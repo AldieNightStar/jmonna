7b03d45df2ea00a7d3b5d0529de47337a46e707d
AldieNightStar <AldieNightStar@gmail.com>
2024-08-17 02:24:22 +0200

    Small fixes

--- src/main/kotlin/haxidenti/jmonna/service/WsBroadcaster.kt
+++ src/main/kotlin/haxidenti/jmonna/service/WsBroadcaster.kt
@@ -2,14 +2,20 @@ package haxidenti.jmonna.service
 
 import io.javalin.websocket.WsConfig
 import io.javalin.websocket.WsContext
+import java.time.Duration
 
 class WsBroadcaster {
-    private val connections = mutableSetOf<WsContext>()
+    private val connections = mutableListOf<WsContext>()
 
     fun serve(w: WsConfig) {
-        w.onConnect { ws -> connections.add(ws) }
+        w.onConnect { ws ->
+            ws.session.policy.idleTimeout = Duration.ofHours(1)
+            connections.add(ws)
+        }
         w.onClose { ws -> connections.remove(ws) }
-        w.onMessage { ws -> broadCast(ws, ws.message()) }
+        w.onMessage { ws ->
+            broadCast(ws, ws.message())
+        }
 
         // Print to console any errors
         w.onError { ws -> ws.error()?.printStackTrace() }
